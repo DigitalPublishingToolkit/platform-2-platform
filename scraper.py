8f313a8b9383c3b9ed2cc196d34930f770985042
afincato <work@andrefincato.info>
2018-10-30 00:04:47 +0100

    add csv export support

--- scraper.py
+++ scraper.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import redis
+import nltk
+from nltk.tag import pos_tag_sents
+
+import io
+import csv
 
 db = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
 
 #-- get sitemap
 
 url = 'https://amateurcities.com/post-sitemap.xml'
+url2 = 'https://www.unstudio.com/sitemap.xml'
 r = requests.get(url)
 data = r.text
 
@@ -29,9 +35,9 @@ for item in soup.find_all('lastmod'):
 index = dict(zip(mod, url))
 
 db.hmset('sitemap', index)
-# print('--- ↓ db.sitemap ↓ ---')
-# print(db.hgetall('sitemap'))
-# print('--- ↑ db.sitemap ↑ ---')
+print('--- ↓ db.sitemap ↓ ---')
+print(db.hgetall('sitemap'))
+print('--- ↑ db.sitemap ↑ ---')
 
 #-- fetch all pages and save them in the db
 
@@ -45,37 +51,55 @@ db.hmset('sitemap', index)
 # }]
 
 with requests.Session() as s:
+
+  output = io.StringIO()
+  f = csv.writer(open('dump.csv', 'w'))
+  f.writerow(['mod', 'url', 'title', 'desc', 'tags', 'section', 'body', 'body-tokens'])
+  writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
+
   for mod, url in index.items():
     #-- if lastmod is newer than prev lastmod
-    article = s.get(url)
-    soup = BeautifulSoup(article.text, 'lxml')
+    art = s.get(url)
+    soup = BeautifulSoup(art.text, 'lxml')
 
     #-- extract infos and make dict
     article = {}
 
     article['mod'] = mod
+    #f.writerow([mod])
     article['url'] = url
-    
+ 
     title = soup.find('title').text
     article['title'] = title
-    #print('title= ' + title)
 
     desc = soup.find(attrs={'property':'og:description'}).get('content')
     article['desc'] = desc
-    #print('desc= ' + desc)
 
     tag = soup.find(attrs={'property':'article:tag'})
     if (tag != None):
         tag = tag.get('content')
         article['tag'] = tag
-        #print('tags= ' + tag) 
 
     section = soup.find(attrs={'property':'article:section'})
     if (section != None):
         section = section.get('content')
         article['section'] = section
-        #print('section= ' + section + '\n\n') 
- 
+
+    body = soup.find('article')
+    if (body != None):
+        pp = soup.find_all('p')
+        copy = []
+        for p in pp:
+            copy.append(p.text)
+        copy = "".join(copy)
+        article['body'] = copy
+
+        cptk = nltk.word_tokenize(copy)
+        cptg = nltk.pos_tag(cptk)
+        article['body-tokens'] = cptg
+
+        f.writerow([mod, url, title, desc, tag, section, copy, cptg])
+        
     #-- save to db
     db.hmset('entry', article)
     print('--- ↓ entry ↓ ---')
