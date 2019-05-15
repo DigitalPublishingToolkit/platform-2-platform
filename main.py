5e480a3832da420f128712cdc268c262aa93297a
afincato <work@andrefincato.info>
2019-05-15 15:03:13 +0200

    moving scraper.py into main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import sys
 import time
 
+import requests
+from bs4 import BeautifulSoup
+import json
+import markdown
+
+from acoo_scraper import ac_oo_scraper
+import os_scraper
+import osr_scraper
+
 from scraper import scrapple
 from text_processing import text_cu, stop_words, word_freq, phrases_freq, relevancy
 import nltk
 from nltk.stem import WordNetLemmatizer
-
-import io
-import csv
 
 #----
 # run scraper with list of urls to check for scraping
@@ -18,9 +24,6 @@ import csv
 #   maybe save the timestamp of latest sitemap checkup
 
 def main(name):
-  #-- scrape article
-  article = scrapple(name)
-
   #-- shape data up
   def text_processing (article):
     words = text_cu(article['body'])
@@ -41,30 +44,87 @@ def main(name):
     print('text processing done...')
     return article
 
-  try:
-    article = text_processing(article)
-  except:
-    print('article has no `body` field')
-
-  #-- save it to db
+  #-- save data to db
   def save (article):
-    name = names[sys.argv[1]]
     timestamp = time.strftime("%Y-%m-%d-%H%M%S")
     filename = name + '_' + timestamp
 
-    output = io.StringIO()
-
-    f = csv.writer(open('dump/%s.csv' % filename, 'w'))
-    f.writerow(['mod', 'url', 'title', 'publisher', 'abstract', 'tags', 'author', 'section', 'body', 'body-tokens', 'body-words-length', 'word-freq', '2-word phrases', '3-word phrases', 'relevancy'])
-    writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
-
-    f.writerow(article.values())
-
-    with open('dump/%s.json' % filename, 'w') as fp:
-      json.dump(article, fp)
-
+    print(article)
     print('saving done...')
 
+  #-- scrape article
+  def scraple (name):
+    #-- get sitemap
+    sitemap = {
+      'ac': 'https://amateurcities.com/post-sitemap.xml',
+      'oo': 'https://www.onlineopen.org/sitemap.xml',
+      'os': 'http://hub.openset.nl/backend/wp-json',
+      'osr': 'http://openset.nl/reader/pocket/api/get.php?type=root&id=root'
+    }
+
+    names = {
+      'ac': 'amateurcities',
+      'oo': 'online-open',
+      'os': 'openset',
+      'osr': 'openset-reader'
+    }
+
+    t_url = sitemap[name]
+    r = requests.get(t_url)
+    data = r.text
+
+    #-- make dict with { <lastmod>: <url> }
+    soup = BeautifulSoup(data, "lxml")
+
+    url = []
+    mod = []
+
+    def sitemap (data, key, arr):
+      for item in data.find_all(key):
+        arr.append(item.text)
+
+    sitemap(soup, 'loc', url)
+    sitemap(soup, 'lastmod', mod)
+
+    index = dict(zip(mod, url))
+
+    #-- TODO compare sitemap with db
+    # for mod, url in index.items():
+    #   if url is not in db.xx.url:
+    #     if mod > db.xx.date
+    #       → do scrape()
+
+    #--- scraping
+    with requests.Session() as s:
+      print('scraping ✂︎')
+
+      article = {}
+      #-- go through each link in sitemap
+      # ac / oo
+      if (name == 'ac' or name == 'oo'):
+        for mod, url in index.items():
+          # 1. scrape
+          ac_oo_scraper(s, mod, url, names[name], article)
+
+          # 2. process
+          try:
+            article = text_processing(article)
+          except:
+            print('article has no `body` field')
+
+          # 3. save to db
+          save(article)
+
+      # os
+      elif (t_url == 'os'):
+        for item in apis['sections']:
+          os_scraper(item)
+
+      #-- end
+      print('scraping completed!!')
+      return article
+
+  # article = scrapple(name)
 
 if __name__ == '__main__':
   main(sys.argv[1])
