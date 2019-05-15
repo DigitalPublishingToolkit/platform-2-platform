1ebaf1ef5c5e7eb9dd877c554d2da508ab42705a
afincato <work@andrefincato.info>
2019-05-15 15:10:38 +0200

    main.py now works on a per article level

--- main.py
+++ main.py
@@ -23,108 +23,99 @@ from nltk.stem import WordNetLemmatizer
 #   add them to the db, else do nothing
 #   maybe save the timestamp of latest sitemap checkup
 
+#-- shape data up
+def text_processing (article):
+  words = text_cu(article['body'])
+  words = nltk.word_tokenize(words)
+
+  lemmatizer = WordNetLemmatizer()
+  words = [lemmatizer.lemmatize(word) for word in words]
+
+  stop_words(words, article)
+
+  word_freq(article['body-tokens'], article)
+
+  phrases_freq(article['body-tokens'], 2, article)
+  phrases_freq(article['body-tokens'], 3, article)
+
+  relevancy(article['word-freq'], article)
+
+  print('text processing done...')
+  return article
+
+#-- save data to db
+def save (article):
+  timestamp = time.strftime("%Y-%m-%d-%H%M%S")
+  print(article)
+  print('saving done...')
+
 def main(name):
-  #-- shape data up
-  def text_processing (article):
-    words = text_cu(article['body'])
-    words = nltk.word_tokenize(words)
+  #-- get sitemap
+  sitemap = {
+    'ac': 'https://amateurcities.com/post-sitemap.xml',
+    'oo': 'https://www.onlineopen.org/sitemap.xml',
+    'os': 'http://hub.openset.nl/backend/wp-json',
+    'osr': 'http://openset.nl/reader/pocket/api/get.php?type=root&id=root'
+  }
 
-    lemmatizer = WordNetLemmatizer()
-    words = [lemmatizer.lemmatize(word) for word in words]
+  names = {
+    'ac': 'amateurcities',
+    'oo': 'online-open',
+    'os': 'openset',
+    'osr': 'openset-reader'
+  }
 
-    stop_words(words, article)
+  t_url = sitemap[name]
+  r = requests.get(t_url)
+  data = r.text
 
-    word_freq(article['body-tokens'], article)
+  #-- make dict with { <lastmod>: <url> }
+  soup = BeautifulSoup(data, "lxml")
 
-    phrases_freq(article['body-tokens'], 2, article)
-    phrases_freq(article['body-tokens'], 3, article)
+  url = []
+  mod = []
 
-    relevancy(article['word-freq'], article)
+  def sitemap (data, key, arr):
+    for item in data.find_all(key):
+      arr.append(item.text)
 
-    print('text processing done...')
-    return article
+  sitemap(soup, 'loc', url)
+  sitemap(soup, 'lastmod', mod)
 
-  #-- save data to db
-  def save (article):
-    timestamp = time.strftime("%Y-%m-%d-%H%M%S")
-    filename = name + '_' + timestamp
+  index = dict(zip(mod, url))
 
-    print(article)
-    print('saving done...')
+  #-- compare sitemap with db
+  # for mod, url in index.items():
+  #   if url is not in db.xx.url:
+  #     if mod > db.xx.date
+  #       → do scrape()
 
-  #-- scrape article
-  def scraple (name):
-    #-- get sitemap
-    sitemap = {
-      'ac': 'https://amateurcities.com/post-sitemap.xml',
-      'oo': 'https://www.onlineopen.org/sitemap.xml',
-      'os': 'http://hub.openset.nl/backend/wp-json',
-      'osr': 'http://openset.nl/reader/pocket/api/get.php?type=root&id=root'
-    }
+  #--- scraping + processing + saving
+  with requests.Session() as s:
+    print('scraping ✂︎')
 
-    names = {
-      'ac': 'amateurcities',
-      'oo': 'online-open',
-      'os': 'openset',
-      'osr': 'openset-reader'
-    }
+    article = {}
+    #-- go through each link in sitemap
+    # ac / oo
+    if (name == 'ac' or name == 'oo'):
+      for mod, url in index.items():
+        # 1. scrape
+        ac_oo_scraper(s, mod, url, names[name], article)
+        print('scraping completed!!')
 
-    t_url = sitemap[name]
-    r = requests.get(t_url)
-    data = r.text
+        # 2. process
+        try:
+          article = text_processing(article)
+        except:
+          print('article has no `body` field')
 
-    #-- make dict with { <lastmod>: <url> }
-    soup = BeautifulSoup(data, "lxml")
+        # 3. save to db
+        save(article)
 
-    url = []
-    mod = []
-
-    def sitemap (data, key, arr):
-      for item in data.find_all(key):
-        arr.append(item.text)
-
-    sitemap(soup, 'loc', url)
-    sitemap(soup, 'lastmod', mod)
-
-    index = dict(zip(mod, url))
-
-    #-- TODO compare sitemap with db
-    # for mod, url in index.items():
-    #   if url is not in db.xx.url:
-    #     if mod > db.xx.date
-    #       → do scrape()
-
-    #--- scraping
-    with requests.Session() as s:
-      print('scraping ✂︎')
-
-      article = {}
-      #-- go through each link in sitemap
-      # ac / oo
-      if (name == 'ac' or name == 'oo'):
-        for mod, url in index.items():
-          # 1. scrape
-          ac_oo_scraper(s, mod, url, names[name], article)
-
-          # 2. process
-          try:
-            article = text_processing(article)
-          except:
-            print('article has no `body` field')
-
-          # 3. save to db
-          save(article)
-
-      # os
-      elif (t_url == 'os'):
-        for item in apis['sections']:
-          os_scraper(item)
-
-      #-- end
-      print('scraping completed!!')
-      return article
-
-  # article = scrapple(name)
+    # os
+    elif (t_url == 'os'):
+      for item in apis['sections']:
+        os_scraper(item)
 
 if __name__ == '__main__':
   main(sys.argv[1])
