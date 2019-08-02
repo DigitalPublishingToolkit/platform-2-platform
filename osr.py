f8fac008629d7a4efb23342808674becee2ee164
afincato <work@andrefincato.info>
2019-08-02 16:22:11 +0200

    clean up

--- osr.py
+++ osr.py
@@ -6,10 +6,8 @@ from bs4 import BeautifulSoup
 def scraper(s, slug, article):
   art = s.get('http://openset.nl/reader/pocket/api/get.php?type=articles&id=' + slug)
   entry = art.json()
-  print(slug)
   # scheme 2016-04-14T21:11:06+00:00
   article['mod'] = datetime.now(timezone.utc).replace(microsecond=0).isoformat()
-  print(article['mod'])
   article['url'] = 'http://openset.nl/reader/#!/article/' + slug
 
   article['title'] = entry['title']
@@ -31,6 +29,5 @@ def scraper(s, slug, article):
   copy = ''.join(copy)
   article['body'] = copy
 
-  # print(article)
   print('scraping done...')
   return article
