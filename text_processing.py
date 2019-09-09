d77da046044298641ac78a1e3b4668ae413d24e1
afincato <work@andrefincato.info>
2019-09-09 15:36:10 +0200

    clean up old functions

--- text_processing.py
+++ text_processing.py
@@ -35,7 +35,7 @@ def pos(corpus, article):
   words = []
   # take out adverbs, but why?
   for word, code in tk:
-    if (code != 'RB'):
+    # if (code != 'RB'):
       words.append(word)
 
   return words
@@ -80,42 +80,3 @@ def tags_filter(tags):
 
   return taglist
 
-
-#-- gonna replace both `word-frequency`,
-# `n-word phrases frequency` and
-# `relevancy` with `word2vec` and `doc2vec`
-# using `gensim`
-
-#-- word-frequency
-def word_freq(corpus, article):
-  wordfreq = []
-  wf = FreqDist(corpus)
-
-  for word, freq in wf.most_common():
-    # print(word, freq)
-
-    # (word-frequency / body-tokens-length ) * 100
-    rel = (freq / len(corpus)) * 100
-    wwf = word, freq, rel
-
-    wordfreq.append(wwf)
-
-  article['word-freq'] = wordfreq
-
-
-#-- n-word phrases frequency
-def phrases_freq(text, size, article):
-  pf = dict()
-  pf = FreqDist(ngrams(text, size))
-
-  article[str(size) + 'w-phrases'] = pf.most_common()
-
-
-def relevancy(word_freq, article):
-  relevancy = 0
-  addup = 0
-  for word in word_freq:
-    addup += word[2]
-    relevancy = addup / len(word_freq)
-
-  article['relevancy'] = relevancy
