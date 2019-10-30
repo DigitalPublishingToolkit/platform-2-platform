2d348a878e9f816541f9363cf6ab6dad61288106
afincato <work@andrefincato.info>
2019-10-30 13:42:42 +0100

    return n results based on rate value

--- ask.py
+++ ask.py
@@ -73,32 +73,41 @@ def ask(title, publisher, article_id, labels):
       td = TaggedDocument(article['tokens'], 1)
 
       inferred_vector = model.infer_vector(td[0])
-      sims = model.docvecs.most_similar([inferred_vector])
+      #-- we get our most-similar results as documents,
+      #-- we set `topn` to return the n of results we want to have
+      sims = model.docvecs.most_similar([inferred_vector], topn=len(documents))
+
+      print('SIMS', sims, '\n')
+      # print('DOC', documents, '\n')
+      print('DOC-LEN', len(documents), '\n')
 
       results = []
-      for label, index in [('MOST', 0), ('SECOND-MOST', 1), ('THIRD-MOST', 2)]:
-        mod = metadata[documents[index].tags[0]]['mod'],
-        url = metadata[documents[index].tags[0]]['url'],
-        title = metadata[documents[index].tags[0]]['title']
-        publisher = metadata[documents[index].tags[0]]['publisher']
-        abstract = metadata[documents[index].tags[0]]['abstract']
-        tags = metadata[documents[index].tags[0]]['tags']
-        author = metadata[documents[index].tags[0]]['author']
-        body = metadata[documents[index].tags[0]]['body']
-        article_id = metadata[documents[index].tags[0]]['id']
+      # for label, index in [('MOST', 0), ('SECOND-MOST', 1), ('THIRD-MOST', 2)]:
+      for index, (tag_id, rate) in enumerate(sims):
+        if (rate >= 0.1):
+          print(index, tag_id, rate)
+          mod = metadata[documents[index].tags[0]]['mod'],
+          url = metadata[documents[index].tags[0]]['url'],
+          title = metadata[documents[index].tags[0]]['title']
+          publisher = metadata[documents[index].tags[0]]['publisher']
+          abstract = metadata[documents[index].tags[0]]['abstract']
+          tags = metadata[documents[index].tags[0]]['tags']
+          author = metadata[documents[index].tags[0]]['author']
+          body = metadata[documents[index].tags[0]]['body']
+          article_id = metadata[documents[index].tags[0]]['id']
 
-        article = {
-            "mod": mod[0],
-            "url": url[0],
-            "title": title,
-            "publisher": publisher,
-            "abstract": abstract,
-            "tags": tags,
-            "author": author,
-            "body": body,
-            "id": article_id,
-            "score": sims[index][1]
-        }
+          article = {
+              "mod": mod[0],
+              "url": url[0],
+              "title": title,
+              "publisher": publisher,
+              "abstract": abstract,
+              "tags": tags,
+              "author": author,
+              "body": body,
+              "id": article_id,
+              "score": rate
+           }
         results.append(article)
 
       return results
