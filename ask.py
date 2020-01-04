3eeedf3c83aa4a64b30e91234c932f5c7552f08c
afincato <work@andrefincato.info>
2020-01-04 12:11:15 +0100

    made funcs and clean up

--- ask.py
+++ ask.py
@@ -4,97 +4,119 @@ import text_processing
 
 #-- main ref <https://radimrehurek.com/gensim/auto_examples/tutorials/run_doc2vec_lee.html>
 
+def model_setup(documents, fname):
+  model = Doc2Vec(vector_size=50, min_count=2, epochs=40)
+  print('model initialized', model)
+  model.build_vocab(documents)
+
+  # save model to disk
+  model.save(fname)
+  return model
+
+def get_pubs(pub):
+  pubs = ['amateur-cities', 'online-open', 'open-set-reader']
+  pubs.remove(pub)
+  fn_model = '_'.join(pubs)
+  return fn_model
+
+def get_article_vocab(tokens, model):
+  model_vocab = [word for word in model.wv.vocab]
+  s_model_tk = set(model_vocab)
+
+  article_tokens = []
+  for token in tokens:
+    for item in token:
+      if type(item) is list:
+          article_tokens.append(','.join(item))
+      else:
+        article_tokens.append(item)
+
+  s_article_tk = set(article_tokens)
+  article_vocab = s_article_tk.intersection(s_model_tk)
+
+  return list(article_vocab)
+
 def ask(title, publisher, article_id, labels):
-    # -- get corpuses from all pubs except the one passed as `arg`
-    input_corpus = get_from_db.get_corpus(publisher, **labels)
+  # -- get corpuses from all pubs except the one passed as `arg`
+  input_corpus = get_from_db.get_corpus(publisher, **labels)
 
-    #-- instead of adding indexical numbers to the tag section of the `TaggedDocument`,
-    # let's add the article['artid'] for later cross retrieval when having to make the final list of similar articles by pulling from db.metadata
-    documents = [TaggedDocument(doc['tokens'], [doc['artid']]) for i, doc in enumerate(input_corpus['data'])]
-    print('DOCUMENTS', documents)
+  #-- instead of adding indexical numbers to the tag section of the `TaggedDocument` (it's a tuple: `Taggeddocument(docs, tags)`),
+  # let's add the article['artid'] for later cross retrieval when having to make the final list of similar articles by pulling from db.metadata
+  documents = [TaggedDocument(doc['tokens'], [doc['artid']]) for i, doc in enumerate(input_corpus['data'])]
+  # print('DOCUMENTS', documents)
 
-    # pubs = ['amateur-cities', 'online-open', 'open-set-reader']
-    # pubs.remove(publisher)
-    # fn_model = '_'.join(pubs)
+  #-- get list of pubs except the one passed as `arg` to ask()
+  fn_model = get_pubs(publisher)
 
-    model = Doc2Vec(vector_size=50, min_count=2, epochs=40)
-    print('model initialized', model)
-    model.build_vocab(documents)
-
+  #-- check if having to build new model again by loading model saved to disk, if it fails build model anew and train it first
+  try:
+    model = Doc2Vec.load(fn_model)
+  except Exception as e:
+    print('model could not be loaded', e)
+    model = model_setup(documents, fn_model)
     model.train(documents, total_examples=model.corpus_count, epochs=model.epochs)
     print('model is training')
 
-    # -- this return word token and representation of it in vector space
-    # my_dict = dict({})
-    # for idx, key in enumerate(model.wv.vocab):
-    #     my_dict[key] = model.wv[key]
-    #     # my_dict[key] = model.wv.get_vector(key)
-    #     # my_dict[key] = model.wv.word_vec(key, use_norm=False)
-    # print(my_dict)
+  #-- convert labels dict to list, pass it to `get_specific_article`
+  article = {}
 
-    #-- convert labels dict to list, pass it to `get_specific_article`
-    article = {}
+  # using the below function does not differentiate enough the results for the algorithm, so we don't use it for now (list of articles is small)
+  # add only fields that are True sent from the /ask request
+  # labels = [k for k, v in labels.items() if v is True]
 
-    # add only fields that are True sent from the /ask request
-    # labels = [k for k, v in labels.items() if v is True]
-    # using the above function does not differentiate enough the results from the algorithm,
-    # so we don't use it for now (list of articles is small)
-    labels = [k for k, v in labels.items()]
+  labels = [k for k, v in labels.items()]
 
-    # get full article from article id passed through /Ask POST
-    words = get_from_db.get_specific_article(article_id, labels)
+  # get full article from article id passed through /Ask POST
+  words = get_from_db.get_specific_article(article_id, labels)
 
-    if bool(words) is False:
-      return {'error': 'article with id %s not found' % article_id}
-    else:
-      text_processing.vector_tokenize(words, article)
-      vector_l = [item for item in article.values()]
+  if bool(words) is False:
+    return {'error': 'article with id %s not found' % article_id}
+  else:
+    text_processing.vector_tokenize(words, article)
+    vector_l = [item for item in article.values()]
 
-    # <https://stackoverflow.com/a/952952>
-    # flat-list
-    vector = []
-    for sublist in vector_l:
-      for item in sublist:
-        vector.append(item)
+  # <https://stackoverflow.com/a/952952>
+  # make flat-list
+  vector = []
+  for sublist in vector_l:
+    for item in sublist:
+      vector.append(item)
 
-    # print('VECTOR', vector)
-    inferred_vector = model.infer_vector(vector)
-    # print('INFERRED_VECTOR', inferred_vector)
+  # print('VECTOR', vector)
+  inferred_vector = model.infer_vector(vector)
+  # print('INFERRED_VECTOR', inferred_vector)
 
-    #-- we get our most-similar results as documents, we set `topn` to return
-    # the n of results we want to have
-    # sims = model.docvecs.most_similar([inferred_vector], topn=len(documents))
-    sims = model.docvecs.most_similar([inferred_vector], topn=100)
+  #-- we get our most-similar results as documents, we set `topn` to return the n of results we want to have
+  # sims = model.docvecs.most_similar([inferred_vector], topn=len(documents))
+  sims = model.docvecs.most_similar([inferred_vector], topn=100)
+  # print('SIMS', sims)
 
-    print('SIMS', sims)
+  # -- get article metadata from all pubs except the one passed as `arg`
+  artids = tuple([item['artid'] for item in input_corpus['data']])
+  metadata = get_from_db.get_metadata_from_artid(publisher, artids)
+  # print('METADATA', metadata)
 
-    # -- get article metadata from all pubs except the one passed as `arg`
-    artids = tuple([item['artid'] for item in input_corpus['data']])
-    metadata = get_from_db.get_metadata_from_artid(publisher, artids)
+  results = []
+  #-- iterate over SIMS and use tag_id (`artid`) to pick the corresponding full article from the metadata list of articles
+  for index, (tag_id, rate) in enumerate(sims):
+    if (rate >= 0.1):
+      print('INDEX, TAG_ID, RATE', index, tag_id, rate)
+      article = metadata[tag_id]
+      print(article['title'], article['publisher'])
 
-    # print('METADATA', metadata)
+      score = get_from_db.get_feedback_match(article_id)
 
-    results = []
-    #-- iterate over SIMS and use tag_id (`artid`) to pick the corresponding
-    # full article from the metadata list of articles
-    for index, (tag_id, rate) in enumerate(sims):
-      if (rate >= 0.1):
-        print('INDEX, TAG_ID, RATE', index, tag_id, rate)
-        article = metadata[tag_id]
+      # add `rate` and `score` to article from metadata
+      article['rate'] = rate
+      article['score'] = score
 
-        score = get_from_db.get_feedback_match(article_id)
+      token_dict = {}
+      tokens = text_processing.process_tokens(article, token_dict)
+      tokens = [v for k, v in tokens.items() if k in labels]
+      vocab = get_article_vocab(tokens, model)
 
-        # add `rate` and `score` to article from metadata
-        article.update({'rate': rate}, {'score': score})
+      article['vocabulary'] = vocab
 
-        # token_dict = {}
-        # tokens = text_processing.process_tokens(article, token_dict)
-        # tokens = [v for k, v in tokens.items() if k in labels]
-        # # print('TOKENS FILTERED', tokens)
-        # vocab = get_article_vocab(tokens)
+    results.append(article)
 
-        # article['vocabulary'] = vocab
-
-      results.append(article)
-
-    return results
+  return results
